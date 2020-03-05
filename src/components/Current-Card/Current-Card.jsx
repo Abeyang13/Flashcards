8a5585abc08bc6752027ed7403fb1b3bf3d0e30d
iaiir <abeyang13@gmail.com>
2020-03-05 13:09:56 -0600

    created a Container for CurrentCard with Rows and Colums.

--- src/components/Current-Card/Current-Card.jsx
+++ src/components/Current-Card/Current-Card.jsx
@@ -1,4 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import './Current-Card.css';
 
 const CurrentCard = (props) => {
@@ -7,14 +10,22 @@ const CurrentCard = (props) => {
             <div className="subject-header">
                 <h1>{props.title}</h1>
             </div>
-            <div className="main">
-                <div className="front">
-                    {props.word}
-                </div>
-                <div className="back">
-                    {props.definition}
-                </div>
-            </div>
+            <Container>
+                <Row>
+                    <Col></Col>
+                    <Col>
+                        <div className="main">
+                            <div className="front">
+                                {props.word}
+                            </div>
+                            <div className="back">
+                                {props.definition}
+                            </div>
+                        </div>
+                    </Col>
+                    <Col></Col>
+                </Row>
+            </Container>
         </div>
     );
 }
